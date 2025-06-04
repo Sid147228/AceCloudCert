e6746a7310e1e06e4482dca36aa26cc18570dd2e
Sid147228 <mailsiddharth17@gmail.com>
2025-06-05 00:31:09 +0100

    Initial commit from Replit-ready codebase

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import BottomTabNavigator from './src/navigation/BottomTabNavigator';
-import { TailwindProvider } from 'tailwindcss-react-native';
 import { SafeAreaView, View } from 'react-native';
-import CookieBanner from './src/components/CookieBanner';
+import BottomTabNavigator from './src/navigation/BottomTabNavigator';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <TailwindProvider>
-        <SafeAreaView className="flex-1">
-          <View className="flex-1">
-            <BottomTabNavigator />
-            <CookieBanner /> {/* 👈 Shown at bottom of all screens */}
-          </View>
-        </SafeAreaView>
-      </TailwindProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+          <BottomTabNavigator />
+        </View>
+      </SafeAreaView>
     </NavigationContainer>
   );
 }
